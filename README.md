c71955f371f18edb55a7fcf27097c06e049e5144
Irum Jahan Indrabi <syedirumjahan@gmail.com>
2026-01-07 19:42:24 +0000

    update README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Python-
+#add two numbers
+a=5
+b=2
+x=a+b
+print(x)
---
+++ test.py
@@ -0,0 +1,6 @@
+
+
+r=int(input("Enter a number: "))
+import math
+area=math.pi*r**2
+print("Area of circle with radius",r,"is:",area)
